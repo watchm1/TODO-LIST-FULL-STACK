2b2bde28b26a1b6bb4dae3a9058cf91ff8982131
watchm1 <m.ali.software.dev@gmail.com>
2023-04-15 03:23:04 +0300

    updated

---
+++ Server/Core.services/Utils/HTTP/Response.php
@@ -0,0 +1,54 @@
+<?php
+namespace Core\Utils\HTTP\Response;
+
+class Response {
+    private array $data = array();
+    private int $status = 200;
+    private array $responseHeaders = array();
+    private array $cookies = array();
+
+    public function SetData($data):Response {
+        $this->$data = $data;
+        return $this;
+    }
+    public function SetHeader($headers):Response {
+        $this->responseHeaders = $headers;
+        return $this;
+    }
+    public function SetStatus($status): Response {
+        $this->status = $status;
+        return $this;
+    }
+    public function SetCookie($name, $value="", $expires_or_options=0, $path="", $domain="", $secure=false, $httponly=false):void
+    {
+        $this->cookies[] = array(
+            'name' => $name,
+            'value' => $value,
+            'maxAge' => $expires_or_options,
+            'path' => $path,
+            'domain' => $domain,
+            'secure' => $secure,
+            'httpOnly' => $httponly
+        );
+    }
+    public function Send():void
+    {
+        http_response_code($this->status);
+        foreach ($this->responseHeaders as $name => $value)
+        {
+            header("$name: $value");
+        }
+        foreach ($this->cookies as $cookie)
+        {
+            setcookie(
+                $cookie['name'],
+                $cookie['value'],
+                time() + $cookie['maxAge'],
+                $cookie['path'],
+                $cookie['secure'],
+                $cookie['httpOnly']
+            );
+        }
+        echo json_encode($this->data);
+    }
+}
