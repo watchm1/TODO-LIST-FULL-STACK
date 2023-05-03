e7b2d11dd283f8719833da3a5087d3eb68398ae7
watchm1 <m.ali.software.dev@gmail.com>
2023-05-03 22:24:38 +0300

    updated

--- Server/Core/Storage/Virtual/Base/IRepository.php
+++ Server/Core/Storage/Virtual/Base/IRepository.php
@@ -1,13 +1,34 @@
 <?php
 namespace Core\Storage\Virtual\Base;
 /**
- * @template
+ * @template T of IEntity
  */
 interface IRepository
 {
+    /**
+     * @return array<T>
+     */
     public function GetAll(): array;
-    public function GetOne(): IEntity;
-    public function CreateOne(IEntity $entity): IEntity;
-    public function UpdateOne(IEntity $updatedEntity): IEntity;
-    public function Delete(int $id):bool;
+
+    /**
+     * @return T
+     */
+    public function GetOne(): mixed;
+
+    /**
+     * @param T $entity
+     * @return T
+     */
+    public function CreateOne($entity): mixed;
+
+    /**
+     * @param T $updatedEntity
+     * @return IEntity
+     */
+    public function UpdateOne($updatedEntity): mixed;
+
+    /**
+     * @param int $id
+     */
+    public function Delete(int $id);
 }
